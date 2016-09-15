5dcc9ef4e3e36600e9116b8bbc598521d111d798
nalread <nalread@gmail.com>
2016-09-15 17:26:14 +0200

    Restructure loop, 'run' variable made obsolete

--- shopping_list.py
+++ shopping_list.py
@@ -1,16 +1,18 @@
 prompt = ("> ")
 my_list = []
 action = None
-run = True
+# run = True
 name = input("Hello user. What is your name?\n" + prompt)
 
 print("It's nice to meet you, {}. What would you like to add to your \
 list? (Input DONE to finish.)".format(name))
 
-while run == True:
+# while run == True:
+while True:
     action = input(prompt)
     if action == "DONE":
-        run = False
+        break
+    # run = False
     else:
         my_list.append(action)
 print(my_list)
