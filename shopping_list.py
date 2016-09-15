e040a93178cd074eb8400497bf2995d26b93ef9d
nalread <nalread@gmail.com>
2016-09-15 17:36:15 +0200

    Change finishing print statement to print items one by one, not in a list

--- shopping_list.py
+++ shopping_list.py
@@ -4,7 +4,7 @@ action = None
 name = input("Hello user. What is your name?\n" + prompt)
 
 print("It's nice to meet you, {}. What would you like to add to your \
-list? (Input DONE to finish.)".format(name))
+list?\n(Input DONE to finish)".format(name))
 
 while True:
     action = input(prompt)
@@ -12,4 +12,5 @@ while True:
         break
     else:
         my_list.append(action)
-print(my_list)
+for item in my_list:
+    print(item)
