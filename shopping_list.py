d11194ba8f76b8a89a0e3dd15e03b376778f6e16
nalread <nalread@gmail.com>
2016-09-15 20:14:09 +0200

    Add HELP and SHOW functionality along with a few cosmetical changes

--- shopping_list.py
+++ shopping_list.py
@@ -3,14 +3,29 @@ my_list = []
 action = None
 name = input("Hello user. What is your name?\n" + prompt)
 
+def print_em_all():
+    print ("Here are currently listed items:")
+    for item in my_list:
+        print(item)
+
+def show_help():
+    print ("""Here's an available list of commands:
+    SHOW - shows current contents of the list;
+    DONE - shows current contents of the list and terminates the program;
+    HELP - directs you to this screen""")
+
 print("It's nice to meet you, {}. What would you like to add to your \
-list?\n(Input DONE to finish)".format(name))
+list?\n(Type HELP for list of commands)".format(name))
 
 while True:
     action = input(prompt)
-    if action == "DONE":
+    if action == "SHOW":
+        print_em_all()
+    elif action == "HELP":
+        show_help()
+    elif action == "DONE":
         break
     else:
         my_list.append(action)
-for item in my_list:
-    print(item)
+
+print_em_all()
