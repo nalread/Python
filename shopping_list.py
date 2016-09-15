3c37559fa54eb2ddaf153cbf4d1932c95bab9766
nalread <nalread@gmail.com>
2016-09-15 17:08:15 +0200

    Tidy up main loop

--- shopping_list.py
+++ shopping_list.py
@@ -1,15 +1,22 @@
 prompt = ("> ")
 my_list = []
 action = None
+run = True
 name = input("Hello user. What is your name?\n" + prompt)
 
 print("It's nice to meet you, {}. What would you like to add to your \
 list? (Input DONE to finish.)".format(name))
 
-while action != "DONE":
+# while action != "DONE":
+#     action = input(prompt)
+#     if action == "DONE":
+#         break
+#     else:
+#         my_list.append(action)
+while run == True:
     action = input(prompt)
     if action == "DONE":
-        break
+        run = False
     else:
         my_list.append(action)
 print(my_list)
