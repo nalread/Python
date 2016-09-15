47ff0f4823484f339d672f538f000c1b5ad8bcf3
nalread <nalread@gmail.com>
2016-09-15 17:11:32 +0200

    Delete obsolete loop

--- shopping_list.py
+++ shopping_list.py
@@ -7,12 +7,6 @@ name = input("Hello user. What is your name?\n" + prompt)
 print("It's nice to meet you, {}. What would you like to add to your \
 list? (Input DONE to finish.)".format(name))
 
-# while action != "DONE":
-#     action = input(prompt)
-#     if action == "DONE":
-#         break
-#     else:
-#         my_list.append(action)
 while run == True:
     action = input(prompt)
     if action == "DONE":
