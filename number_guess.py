fb45b41d5a572ed615b8fe77c659991f871dd1be
nalread <nalread@gmail.com>
2016-09-16 19:17:44 +0200

    Add replay value, additional feedback and limited number of guesses

--- number_guess.py
+++ number_guess.py
@@ -2,18 +2,36 @@ import random
 
 number = random.randint(1, 10)
 
-while True:
+def play_the_game():
+    guesses = 0
     print ("I'm thinking about a number between 1 and 10. What's your guess?")
-    try:
-        guess = int(input("> "))
-    except ValueError:
-        print ("That's not a valid number!")
-    else:
-        if guess > number:
-            print ("Too high")
-        elif guess < number:
-            print ("Too low")
+    while guesses < 5:
+        try:
+            guess = int(input("> "))
+        except ValueError:
+            print ("That's not a valid number!")
         else:
-            break
+            if guess > number:
+                print ("Too high")
+                guesses += 1
+            elif guess < number:
+                print ("Too low")
+                guesses += 1
+            else:
+                break
 
-print ("Grats, you won!")
+    if guesses < 5:
+        print ("Grats, you won!")
+    else:
+        print ("Boo, you lost! My number was {}!".format(number))
+
+play_the_game()
+
+while True:
+    print ("Do you want to play again?")
+    answer = input("(Y)es or (N)o\n>").lower()
+    if answer == 'y':
+        play_the_game()
+    elif answer == 'n':
+        input("Bye!")
+        quit()
