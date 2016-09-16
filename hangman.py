d6e69143fc3b0dc16c8af6ac8f3d73b8aec222b7
nalread <nalread@gmail.com>
2016-09-16 22:48:11 +0200

    Add limit to characters guessed at a time, cleaned out obsolete comments

--- hangman.py
+++ hangman.py
@@ -1,35 +1,33 @@
 import random
 
-# create wordlist
 word_list = [
 'banana',
 'mom',
 'bang',
 'bangity'
 ]
-# choose random word from wordlist
 word = word_list[random.randint(0, len(word_list) - 1)]
 word_shown = []
 index = 0
-guesses = 0
-# create a list with word_shown with underscores
 for i in word:
     word_shown.append("_")
 
 print (word_shown)
 
-# loop: check every letter in word against guessed letter. If matches - uncover
-while guesses < 3:
-    # take a guess
+while True:
     guess = input("Guess a single letter\n>").lower()
-    for j in word:
-        if j == guess:
-            word_shown[index] = guess
-            index += 1
-        else:
-            # guesses += 1
-            index += 1
+    if len(guess) > 1:
+        print ("Please enter just one letter at a time!")
+    else:
+        for j in word:
+            if j == guess:
+                word_shown[index] = guess
+                index += 1
+            else:
+                index += 1
 
-    print (word_shown)
-    index = 0
-# it by altering underscore with the letter
+        print (word_shown)
+        index = 0
+        if "_" not in word_shown:
+            break
+print ("Grats, you won!")
