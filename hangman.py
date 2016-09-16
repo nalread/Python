bbc4f1eaedcdd85ca3c128bbde634a0dbfa94d84
nalread <nalread@gmail.com>
2016-09-16 22:59:17 +0200

    Finally got it to work, put limits on number of guesses, made whole game loopable, added win and lose conditions

--- hangman.py
+++ hangman.py
@@ -6,28 +6,35 @@ word_list = [
 'bang',
 'bangity'
 ]
-word = word_list[random.randint(0, len(word_list) - 1)]
-word_shown = []
-index = 0
-for i in word:
-    word_shown.append("_")
 
-print (word_shown)
+def game():
+    word = word_list[random.randint(0, len(word_list) - 1)]
+    word_shown = []
+    index = 0
+    guesses = len(word) * 3
+    for i in word:
+        word_shown.append("_")
 
-while True:
-    guess = input("Guess a single letter\n>").lower()
-    if len(guess) > 1:
-        print ("Please enter just one letter at a time!")
-    else:
-        for j in word:
-            if j == guess:
-                word_shown[index] = guess
-                index += 1
-            else:
-                index += 1
+    print (word_shown)
+    while guesses > 0:
+        guess = input("Guess a single letter\n>").lower()
+        if len(guess) > 1:
+            print ("Please enter just one letter at a time!")
+        else:
+            for j in word:
+                if j == guess:
+                    word_shown[index] = guess
+                    index += 1
+                    guesses += 1
+                else:
+                    index += 1
+                    guesses -= 1
 
-        print (word_shown)
-        index = 0
-        if "_" not in word_shown:
-            break
-print ("Grats, you won!")
+            print (word_shown)
+            index = 0
+            if "_" not in word_shown:
+                print ("Yay, you won!")
+                quit()
+    print ("Boo, you lost!")
+
+game()
